f5d847020e46a32f7a341148fd5a08ed3d410d45
Aditya Kumar <adk622006@gmail.com>
2025-12-09 14:25:30 +0530

    changes

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const body = await request.json(); // { url: string, shortUrl: string }
   const client = await clientPromise; // connect to mongodb
-  const db = client.db(process.env.MONGODB_DB); // use database named "linkbits"
+  const db = client.db(process.env.MONGODB_DB); // use database from .env
   const collection = db.collection("url"); // use collection named "url"
 
   // check if short url exists in db
