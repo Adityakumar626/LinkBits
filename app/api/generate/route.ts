0db8a3c0c2177a01b6014cdade4fc3b71404512d
Aditya Kumar <adk622006@gmail.com>
2025-12-21 21:54:52 +0530

    conflict error

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -3,6 +3,14 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const body = await request.json(); // { url: string, shortUrl: string }
+
+  if (!body.url || !body.shortUrl) {
+    return NextResponse.json(
+      { message: "URL and ShortUrl are required", success: false, error: true },
+      { status: 400 } // Bad Request if input is missing
+    );
+  }
+
   const client = await clientPromise; // connect to mongodb
   const db = client.db(process.env.MONGODB_DB); // use database from .env
   const collection = db.collection("url"); // use collection named "url"
@@ -10,11 +18,14 @@ export async function POST(request: Request) {
   // check if short url exists in db
   const doc = await collection.findOne({ shortUrl: body.shortUrl }); // this checks if the shortUrl already exists in the database
   if (doc) {
-    return NextResponse.json({
-      message: "short url already exists",
-      success: false,
-      error: true,
-    });
+    return NextResponse.json(
+      {
+        message: "Short URL already exists, try anything else",
+        success: false,
+        error: true,
+      },
+      { status: 409 }
+    );
   }
 
   const result = await collection.insertOne({
